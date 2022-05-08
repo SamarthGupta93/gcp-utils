0c3b1be2689d561a576c380d6f5c130e33f2e619
Samarth Gupta <samarth.bphc@gmail.com>
2022-05-08 05:58:05 -0400

    Helper functions to read and write pandas dataframes

--- gcsutil.py
+++ gcsutil.py
@@ -1,5 +1,6 @@
 import gcsfs
 import pickle
+import pandas as pd
 from google.cloud import storage
 
 class GCSUtil:
@@ -7,9 +8,15 @@ class GCSUtil:
         self.client = storage.Client()
         self.bucket = self.client.get_bucket(bucket_name)
         self.bucket_name = bucket_name
+        self.datapath = "gcs://{}/".format(bucket_name)
         self.fs = gcsfs.GCSFileSystem()
         
-    def upload_df_as_csv(self, df, filepath):
+        
+    def read_csv(self, path):
+        return pd.read_csv(self.datapath + path)
+        
+        
+    def save_df_as_csv(self, df, filepath):
         file_type = filepath.split(".")[-1]
         if file_type=="csv":
             self.bucket.blob(filepath).upload_from_string(df.to_csv(index=False), 'text/csv')
@@ -21,8 +28,9 @@ class GCSUtil:
         with self.fs.open(self.bucket_name + path, 'wb') as f:
             f.write(pickle.dumps(data))
         
+        
     ## Read object
-    def load_from_gcs(self, path):
+    def read_from_gcs(self, path):
         with self.fs.open(self.bucket_name + path, 'rb') as f:
             obj = pickle.loads(f.read())
         return obj 
